ed0c941bda3e6b9c0a094a1f3e6cec5903777702
Pramod Surya <pramoodsurya@gmail.com>
2022-05-31 00:57:07 +0530

    test mail

--- send-mail.php
+++ send-mail.php
@@ -1,54 +1,17 @@
 <?php
 
-	// site owner
-	$site_name = 'vcard HTML5 Template';
-	$sender_domain = 'mailstore1.secureserver.net';
-	$to = 'hello@pramodsurya.in';
-	
-	// contact form fields
-	$name = trim( $_POST['name'] );
-	$email = trim( $_POST['email'] );
-	$subject = trim( $_POST['subject'] );
-	$message = trim( $_POST['message'] );
-	
-	// check for error
-	$error = false;
-	if ( $name === "" ) { $error = true; }
-	if ( $email === "" ) { $error = true; }
-	if ( $subject === "" ) { $error = true; }
-	if ( $message === "" ) { $error = true; }
-	
-	// anti-spam check
-	// http://nfriedly.com/techblog/2009/11/how-to-build-a-spam-free-contact-forms-without-captchas/
-	// if the url field is empty 
-	if(isset($_POST['url']) && $_POST['url'] == ''){
-		 
-		 // if no error, then send mail
-		if ( $error == false )
-		{
-			$body = "Name: $name \n\nEmail: $email \n\nMessage: $message";
-			
-			$headers = "From: " . $site_name . ' <' . $sender_domain . '> ' . "\r\n";
-			$headers .= "Reply-To: " . $name . ' <' . $email . '> ' . "\r\n";
-			
-			$mail_result = mail( $to, $subject, $body, $headers );
-			
-			if ( $mail_result == true )
-				{ echo 'success'; }
-			else
-				{ echo 'unsuccess'; }
-		}
-		else // not validated
-		{
-			echo 'error';
-		}
-		// end if
-		 
-	}
-	else // BOT DETECTED - lets lie to it 
-	{
-		//echo "Thanks, We'll get back to you as soon as possible";
-		echo 'success';
-	}
-	
+if (isset($_POST['submit'])) {
+	$name = $_POST['name'];
+	$mailForm = $_POST['mail'];
+	$message = $_POST['message'];
+
+	$mailTo = "pramoodsurya@gmail.com"
+	$headers = "From: ".$mailFrom;
+	$txt = "You have a message".$name".\n\n".$message;
+
+	mail($mailTo, $name, $txt, $headers);
+
+	header("Location: index.html?MessageSent");
+}
+
 ?>
